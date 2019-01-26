21d737f626407ca59add630caf5c6a392f87fc87
hakaneroztekin <eroztekin@itu.edu.tr>
2019-01-26 16:33:36 +0300

    use lombok in company class

--- src/main/java/com/hakaneroztekin/glassdoorScraper/Company.java
+++ src/main/java/com/hakaneroztekin/glassdoorScraper/Company.java
@@ -1,11 +1,12 @@
 package com.hakaneroztekin.glassdoorScraper;
 
+import lombok.Data;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.stereotype.Component;
 
+@Data
 @Component
 public class Company {
-    Company(){};
     String title;
     Integer rate;
 }
