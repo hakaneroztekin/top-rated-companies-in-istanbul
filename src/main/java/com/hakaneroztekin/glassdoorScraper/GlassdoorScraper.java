9162cdb0d01ceceb5f31716d08b152e859f7461b
hakaneroztekin <eroztekin@itu.edu.tr>
2019-01-26 14:59:45 +0300

    add more dependencies

---
+++ src/main/java/com/hakaneroztekin/glassdoorScraper/GlassdoorScraper.java
@@ -0,0 +1,16 @@
+package com.hakaneroztekin.glassdoorScraper;
+
+import java.math.BigDecimal;
+import java.net.URLEncoder;
+import java.util.List;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
+import com.gargoylesoftware.htmlunit.html.HtmlElement;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+
+
+public class GlassdoorScraper {
+    
+}
