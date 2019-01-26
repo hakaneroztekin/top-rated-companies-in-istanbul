581e9a3c7eb11df685d36d1e0d052029d316d56e
hakaneroztekin <eroztekin@itu.edu.tr>
2019-01-26 14:50:24 +0300

    init spring boot project

---
+++ src/main/java/com/hakaneroztekin/glassdoorScraper/GlassdoorScraperApplication.java
@@ -0,0 +1,14 @@
+package com.hakaneroztekin.glassdoorScraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlassdoorScraperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlassdoorScraperApplication.class, args);
+	}
+
+}
+
