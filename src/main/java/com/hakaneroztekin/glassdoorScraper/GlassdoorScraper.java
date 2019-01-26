477156ca51c258439e0bbc5e7ed4fbb77c6eb2b9
hakaneroztekin <eroztekin@itu.edu.tr>
2019-01-26 15:14:47 +0300

    update classes

--- src/main/java/com/hakaneroztekin/glassdoorScraper/GlassdoorScraper.java
+++ src/main/java/com/hakaneroztekin/glassdoorScraper/GlassdoorScraper.java
@@ -9,8 +9,55 @@ import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
 import com.gargoylesoftware.htmlunit.html.HtmlElement;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GlassdoorScraper implements CommandLineRunner{
+
+    @Override
+    public void run(String [] args) throws Exception{
+        main(args);
+    }
+
+    public static void main(String[] args) {
+        String searchQuery = "iphone 6s" ;
+        String baseUrl = "https://newyork.craigslist.org/" ;
+        WebClient client = new WebClient();
+        client.getOptions().setCssEnabled(false);
+        client.getOptions().setJavaScriptEnabled(false);
+        try {
+            String searchUrl = baseUrl + "search/sss?sort=rel&query=" + URLEncoder.encode(searchQuery, "UTF-8");
+            HtmlPage page = client.getPage(searchUrl);
+
+            List<HtmlElement> items = (List<HtmlElement>) page.getByXPath("//li[@class='result-row']") ;
+            if(items.isEmpty()){
+                System.out.println("No items found !");
+            }else{
+                for(HtmlElement htmlItem : items){
+                    HtmlAnchor itemAnchor = ((HtmlAnchor) htmlItem.getFirstByXPath(".//p[@class='result-info']/a"));
+                    HtmlElement spanPrice = ((HtmlElement) htmlItem.getFirstByXPath(".//a/span[@class='result-price']")) ;
+
+                    // It is possible that an item doesn't have any price, we set the price to 0.0 in this case
+                    String itemPrice = spanPrice == null ? "0.0" : spanPrice.asText() ;
+
+                    Item item = new Item();
+                    item.setTitle(itemAnchor.asText());
+                    item.setUrl( baseUrl + itemAnchor.getHrefAttribute());
+
+                    item.setPrice(new BigDecimal(itemPrice.replace("$", "")));
 
 
-public class GlassdoorScraper {
-    
+                    ObjectMapper mapper = new ObjectMapper();
+                    String jsonString = mapper.writeValueAsString(item) ;
+
+                    System.out.println(jsonString);
+                }
+            }
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
 }
