8eb448546b439509af1b4fb24f3b237fa4d56e47
hakaneroztekin <eroztekin@itu.edu.tr>
2019-01-26 17:10:02 +0300

    move functionality to the method

--- src/main/java/com/hakaneroztekin/glassdoorScraper/GlassdoorScraper.java
+++ src/main/java/com/hakaneroztekin/glassdoorScraper/GlassdoorScraper.java
@@ -29,37 +29,43 @@ public class GlassdoorScraper implements CommandLineRunner{
             String title = userAgent.doc.findFirst("<title>").getChildText(); //get child text of title element.
             System.out.println("\nWebsite: " + title);    //print the title
 
-            Elements companiesHTML = userAgent.doc.findEach("<div class=\"eiHdrModule module snug \"");       //find non-nested tables
-            System.out.println("Found " + companiesHTML.size() + " companies in the page");
+            scrapeCompanies(userAgent);
 
-            List<Company> companies = new ArrayList<>();
-
-            for(Element companyHTML : companiesHTML){
-                Company newCompany = new Company();
-                String companyTitle, companyRate;
-                String companyInfo = companyHTML.getTextContent(); // get company info in a string (a simple approach)
-                String[] splitInfo = companyInfo.trim().split("(Star)");
-
-                String companyTitleAndRate = splitInfo[0]; // get company title and rate. eg: Yapi Kredi 3.6
-                String[] titleAndRate = companyTitleAndRate.split("\\s+"); // split title and rate.
-                String titleAndRateString = String.join(" ", titleAndRate);
-
-                companyRate = titleAndRate[titleAndRate.length-1]; // get rate. eg: 3.6
-                companyTitle = titleAndRateString.toString().replace(companyRate, ""); // Extract the company name. eg: Yapi Kredi
-
-                newCompany.setTitle(titleAndRate[0]); // eg: Vodafone
-                newCompany.setTitle(titleAndRate[1]); // eg: 3.8
-
-                //System.out.println(companyTitleAndRate);
-                System.out.println("Name: " + companyTitle + " Rate: " + companyRate);
-                companies.add(newCompany);
-            }
 
         }
         catch(JauntException e){         //if an HTTP/connection error occurs, handle JauntException.
             System.err.println(e);
         }
 
+    }
+
+    public static void scrapeCompanies(UserAgent userAgent){
+
+        Elements companiesHTML = userAgent.doc.findEach("<div class=\"eiHdrModule module snug \"");       //find non-nested tables
+        System.out.println("Found " + companiesHTML.size() + " companies in the page");
+
+        List<Company> companies = new ArrayList<>();
+
+        for(Element companyHTML : companiesHTML){
+            Company newCompany = new Company();
+            String companyTitle, companyRate;
+            String companyInfo = companyHTML.getTextContent(); // get company info in a string (a simple approach)
+            String[] splitInfo = companyInfo.trim().split("(Star)");
+
+            String companyTitleAndRate = splitInfo[0]; // get company title and rate. eg: Yapi Kredi 3.6
+            String[] titleAndRate = companyTitleAndRate.split("\\s+"); // split title and rate.
+            String titleAndRateString = String.join(" ", titleAndRate);
+
+            companyRate = titleAndRate[titleAndRate.length-1]; // get rate. eg: 3.6
+            companyTitle = titleAndRateString.toString().replace(companyRate, ""); // Extract the company name. eg: Yapi Kredi
+
+            newCompany.setTitle(titleAndRate[0]); // eg: Vodafone
+            newCompany.setTitle(titleAndRate[1]); // eg: 3.8
+
+            //System.out.println(companyTitleAndRate);
+            System.out.println("Name: " + companyTitle + " Rate: " + companyRate);
+            companies.add(newCompany);
+        }
     }
 
 }
