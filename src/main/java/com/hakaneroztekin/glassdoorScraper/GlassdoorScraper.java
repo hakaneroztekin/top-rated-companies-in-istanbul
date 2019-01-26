d00565f39a26b6456376b6fa62f6c7dcf69a1e39
hakaneroztekin <eroztekin@itu.edu.tr>
2019-01-26 16:31:47 +0300

    get company info to a string

--- src/main/java/com/hakaneroztekin/glassdoorScraper/GlassdoorScraper.java
+++ src/main/java/com/hakaneroztekin/glassdoorScraper/GlassdoorScraper.java
@@ -1,9 +1,14 @@
 package com.hakaneroztekin.glassdoorScraper;
 
+import com.jaunt.Element;
+import com.jaunt.Elements;
 import com.jaunt.JauntException;
 import com.jaunt.UserAgent;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+
 
 @Component
 public class GlassdoorScraper implements CommandLineRunner{
@@ -23,11 +28,24 @@ public class GlassdoorScraper implements CommandLineRunner{
             String title = userAgent.doc.findFirst("<title>").getChildText(); //get child text of title element.
             System.out.println("\nWebsite: " + title);    //print the title
 
+            Elements companiesHTML = userAgent.doc.findEach("<div class=\"eiHdrModule module snug \"");       //find non-nested tables
+            System.out.println("Found " + companies.size() + " companies in the page");
+
+            ArrayList<Company> companies;
+
+            for(Element companyHTML : companiesHTML){
+                Company newCompany = new Company();
+                String companyInfo = companyHTML.getTextContent(); // get company info in a string (a simple approach)
+                String[] parts = companyInfo.trim().split("\\d");
+                String companyTitle = parts[0];
+
+            }
 
         }
         catch(JauntException e){         //if an HTTP/connection error occurs, handle JauntException.
             System.err.println(e);
         }
+
     }
 
 }
